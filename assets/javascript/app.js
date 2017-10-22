daf9a56e99e5bac82a1646cd441b675ffc881457
ashdonnelly <ashdonnelly>
2017-10-21 23:14:57 -0600

    forget to git add this whole time; js can't tell if correct answer is checked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,77 @@
-var timer = document.getElementById("timer");
+window.onload = function() {
+	$("#start-button").on("click", function() {
+		$("#start-section").hide();
+		$("#game-content").show();
+		run();
+	});
+};
 
-var sticky = timer.offsetTop;
+// $("#submit-button").on("click", function() {
+// 	// $("#start-section").hide();
+// 	// $("#game-content").hide();
+// 	$("#end-content").show();
+// });
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    timer.classList.add("sticky")
-  } else {
-    timer.classList.remove("sticky");
+// $("#submit-button").on("click", stopTimer, function() {
+// 	$("#end-content").show();
+// 	score;
+// });
+
+var numberTimer = 10;
+
+var correctAnswers = 0;
+
+var missedAnswers = 6;
+
+var intervalID;
+
+// var isCorrect = $(".correct-answer").is(":checked");
+
+$("#correct-answers").text(correctAnswers);
+
+$("#missed-answers").text(missedAnswers);
+
+var answers = $(".answers")
+
+// $("#correct-answer").text(correctAnswer);
+
+function calculateMissed() {
+	missedAnswers = 6 - correctAnswers
+};
+
+function run() {
+    intervalId = setInterval(decrement, 1000);
   }
+
+function endGame() {
+	$("#game-content").hide();
+	$("#end-content").show();
 }
+
+function decrement() {
+	numberTimer--;
+    $("#timer").html(numberTimer);
+      if (numberTimer === 0) {
+        stop();
+        endGame();
+        score();
+      }
+    }
+
+function score() {
+	var selected = false;
+	for (var i = 0; i < answers.length; i++) {
+		if ("#correct-answer".checked) {
+			selected = true;
+			correctAnswers++;
+			console.log("Correct: " + correctAnswers);
+		} else {
+			calculateMissed();
+			console.log("Missed: " + missedAnswers);
+		};
+	};
+};
+
+function stop() {
+	clearInterval(intervalID);
+};
